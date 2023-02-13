7c5e6c3eb0cb3716ed5def31e9b509b9cabfb35f
kunal <kunal.shivnekar@gmail.com>
2023-02-13 20:06:17 +0530

    added algo

--- src/main/kotlin/leetcode/NeetCode.kt
+++ src/main/kotlin/leetcode/NeetCode.kt
@@ -2,6 +2,30 @@ package leetcode
 
 import trees.BinaryTreeNode
 import java.util.*
+import kotlin.math.max
+
+//128. Longest Consecutive Sequence
+fun longestConsecutive(nums: IntArray): Int {
+    if (nums.isEmpty()) return 0
+    if (nums.size == 1) return 1
+    val hashSet = HashSet<Int>()
+    nums.forEach { hashSet.add(it) }
+    var longestSize = 0
+    var isNumberStartOfSequence: Boolean
+    for (num in nums) {
+        isNumberStartOfSequence = !hashSet.contains(num - 1)
+        if (isNumberStartOfSequence) {
+            var nextConsecutiveNumber = num + 1
+            var currentSize = 1
+            while (hashSet.contains(nextConsecutiveNumber)) {
+                nextConsecutiveNumber++
+                currentSize++
+            }
+            longestSize = max(longestSize, currentSize)
+        }
+    }
+    return longestSize
+}
 
 //347. Top K Frequent Elements
 fun topKFrequent(nums: IntArray, k: Int): IntArray {
