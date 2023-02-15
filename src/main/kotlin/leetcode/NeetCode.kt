d493a9eee16ca52f685508c86b6ca4bb196cc733
kunal <kunal.shivnekar@gmail.com>
2023-02-15 10:27:23 +0530

    added algo

--- src/main/kotlin/leetcode/NeetCode.kt
+++ src/main/kotlin/leetcode/NeetCode.kt
@@ -4,6 +4,51 @@ import trees.BinaryTreeNode
 import java.util.*
 import kotlin.math.max
 
+// 33. Search in Rotated Sorted Array
+fun search(nums: IntArray, target: Int): Int {
+    var i = 0
+    var j = nums.size-1
+    var mid = 0
+
+    while(i<=j){
+        mid = i + (j-i)/2
+        if(target == nums[mid]) return mid
+        if(nums[mid]>=nums[i]){
+            if(nums[i] <= target && target < nums[mid])
+                j=mid-1
+            else
+                i = mid+1
+        } else {
+            if(nums[mid] < target && target <= nums[j])
+                i=mid+1
+            else
+                j = mid-1
+        }
+
+    }
+    return -1
+}
+
+
+// 153. Find Minimum in Rotated Sorted Array
+fun findMin(nums: IntArray): Int {
+    var i = 0
+    var j = nums.size-1
+    var min = Integer.MAX_VALUE
+    if((nums[i] < nums[j])|| j==0)
+        return nums[0]
+    while(i<j){
+        var mid = i + (j-i)/2
+        if(nums[mid]>nums[i]){
+            i = mid+1
+        } else {
+            j = mid
+        }
+        min = minOf(min, nums[mid])
+    }
+    return min
+}
+
 //11. Container With Most Water
 fun maxArea(height: IntArray): Int {
     var res = 0
