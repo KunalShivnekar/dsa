d40d24aa7e2f495bab2f63efa22b5cfe378ab866
kunal <kunal.shivnekar@gmail.com>
2023-02-24 18:21:16 +0530

    added topological sort

--- src/main/kotlin/graphs/Topological Sort.kt
+++ src/main/kotlin/graphs/Topological Sort.kt
@@ -1,13 +1,64 @@
 package graphs
 
+
+//210. Course Schedule II // Topological Sort
+class Solution {
+    var order = mutableListOf<Int>()
+    var visited = mutableSetOf<Int>()
+    fun findOrder(numCourses: Int, prerequisites: Array<IntArray>): IntArray {
+        var graph = HashMap<Int, MutableList<Int>>()
+        var cycle = mutableSetOf<Int>()
+
+        prerequisites.forEach{
+            val (a,b) = it
+            graph.getOrPut(a) { mutableListOf<Int>() } += b
+        }
+
+        for(i in 0 until numCourses){
+            if(visited.contains(i)) continue
+            if(dfs(i, graph, cycle).not())
+                return IntArray(0)
+        }
+        return order.toIntArray()
+    }
+
+    fun dfs(course:Int, graph:HashMap<Int,MutableList<Int>>, cycle:MutableSet<Int>):Boolean{
+        var prereqList = graph.getOrPut(course){mutableListOf<Int>()}
+
+        if(cycle.contains(course))
+            return false
+
+        if(prereqList.isEmpty()){
+            order.add(course)
+            visited.add(course)
+            return true
+        }
+
+        cycle.add(course)
+
+        for(i in 0 until prereqList.size){
+            if(visited.contains(prereqList[i])) continue
+            if(dfs(prereqList[i], graph, cycle).not())
+                return false
+        }
+
+        graph[course] = mutableListOf<Int>()
+        cycle.remove(course)
+        order.add(course)
+        visited.add(course)
+        return true
+    }
+}
+
 //207. Course Schedule
 class CourseSchedule {
     fun canFinish(numCourses: Int, prerequisites: Array<IntArray>): Boolean {
         var graph = HashMap<Int, MutableList<Int>>()
         var cycle = mutableSetOf<Int>()
 
-        for(prereq in prerequisites){
-            graph.getOrPut(prereq[0]){mutableListOf<Int>()}.apply{ add(prereq[1])}
+        prerequisites.forEach {
+            val (v, w) = it
+            graph.getOrPut(v) { mutableListOf<Int>() } += w
         }
 
         for(i in 0 until numCourses){
